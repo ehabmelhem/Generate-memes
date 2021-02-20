59008aae68027a2aad4f8ead35346d297cd1b0f9
ehab <Melhemehab@gmail.com>
2021-02-20 21:23:24 +0200

    finish

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,135 @@
 import "./App.css";
-import Meme from "./Meme";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import MobileStepper from "@material-ui/core/MobileStepper";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+const tutorialSteps = [
+  {
+    label: "San Francisco – Oakland Bay Bridge, United States",
+    imgPath:
+      "https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60",
+  },
+  {
+    label: "Bird",
+    imgPath:
+      "https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60",
+  },
+  {
+    label: "Bali, Indonesia",
+    imgPath:
+      "https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80",
+  },
+  {
+    label: "NeONBRAND Digital Marketing, Las Vegas, United States",
+    imgPath:
+      "https://images.unsplash.com/photo-1518732714860-b62714ce0c59?auto=format&fit=crop&w=400&h=250&q=60",
+  },
+  {
+    label: "Goč, Serbia",
+    imgPath:
+      "https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60",
+  },
+];
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 400,
+    flexGrow: 1,
+  },
+  header: {
+    display: "flex",
+    alignItems: "center",
+    height: 50,
+    paddingLeft: theme.spacing(4),
+    backgroundColor: theme.palette.background.default,
+  },
+  img: {
+    height: 255,
+    maxWidth: 400,
+    overflow: "hidden",
+    display: "block",
+    width: "100%",
+  },
+}));
+
 function App() {
   const [memes, setMemes] = useState([]);
+  const classes = useStyles();
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = useState(0);
+  const [maxSteps, setMaxsteps] = useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
   useEffect(async () => {
-    fetch("https://api.imgflip.com/get_memes")
+    await fetch("https://api.imgflip.com/get_memes")
       .then((r) => r.json())
       .then((data) => {
+        console.log(data.data.memes);
         setMemes(data.data.memes);
+        setMaxsteps(data.data.memes.length);
       });
   }, []);
   return (
     <div className="app">
-      <h1>this is meems app</h1>
-      {memes.map((mem, index) => {
-        return <Meme key={index} name={mem.name} url={mem.url} />;
-      })}
+      {memes[0] && (
+        <div className="container">
+          <div className={classes.root}>
+            <Paper square elevation={0} className={classes.header}>
+              <Typography>{memes[activeStep].name}</Typography>
+            </Paper>
+            <img
+              className={classes.img}
+              src={memes[activeStep].url}
+              alt={memes[activeStep].name}
+            />
+            <MobileStepper
+              steps={maxSteps}
+              position="static"
+              variant="text"
+              activeStep={activeStep}
+              nextButton={
+                <Button
+                  size="small"
+                  onClick={handleNext}
+                  disabled={activeStep === maxSteps - 1}
+                >
+                  Next
+                  {theme.direction === "rtl" ? (
+                    <KeyboardArrowLeft />
+                  ) : (
+                    <KeyboardArrowRight />
+                  )}
+                </Button>
+              }
+              backButton={
+                <Button
+                  size="small"
+                  onClick={handleBack}
+                  disabled={activeStep === 0}
+                >
+                  {theme.direction === "rtl" ? (
+                    <KeyboardArrowRight />
+                  ) : (
+                    <KeyboardArrowLeft />
+                  )}
+                  Back
+                </Button>
+              }
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
